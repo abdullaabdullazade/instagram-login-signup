38c64c9ff6d4ba411cb53dfded155c45b65efeeb
unknown <abdulla.abdullazade.2008@gmail.com>
2024-08-16 21:58:08 +0400

    Initial commit
    Generated by create-expo-app 3.0.0.

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,31 @@
+import { Stack } from "expo-router";
+import React from "react";
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Stack
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? "home" : "home-outline"}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Stack>
+  );
+}
