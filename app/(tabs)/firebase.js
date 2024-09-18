8f87297dfa44919eb7def0a504b87d9c1861a192
Abdulla Abdullazade <89068450+abdullaabdullazade@users.noreply.github.com>
2024-09-18 23:16:00 +0400

    Update firebase.js

--- app/(tabs)/firebase.js
+++ app/(tabs)/firebase.js
@@ -3,13 +3,7 @@ import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBU-515LpoouRXRzltgKVi8PSUCj00pte0",
-  authDomain: "instagram-78503.firebaseapp.com",
-  projectId: "instagram-78503",
-  storageBucket: "instagram-78503.appspot.com",
-  messagingSenderId: "984193148709",
-  appId: "1:984193148709:web:bc41625c8fa07c41031ccc",
-  measurementId: "G-0WGZJYJKCD",
+
 };
 
 const firebase = initializeApp(firebaseConfig);
